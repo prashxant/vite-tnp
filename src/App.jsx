0338568c6e84f721c3cfae9042b9abd8fd634d15
Nabadeep <nabadeepdas069@gmail.com>
2024-07-22 14:18:02 +0530

    added src/App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,19 @@
-<<<<<<< HEAD
-import Ourteam from './Ourteam-1';
-=======
-
-import './App.css'
-import React from 'react'
-import Appbar from './Appbar'
+import React from 'react';
+import './App.css';
+import Appbar from './Appbar';
 import Footer from './Footer';
-
->>>>>>> d66197bb226c76df947eb4a4f7c03eb581f59e79
+import Ourteam from './Ourteam-1';
 
 function App() {
   return (
-<<<<<<< HEAD
-    <div className="App">
-      <Ourteam />
-    </div>
-  );
-=======
     <>
-    <Appbar></Appbar> 
-
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-
-<Footer></Footer>
-
+      <Appbar />
+      <div className="App">
+        <Ourteam />
+      </div>
+      <Footer />
     </>
-  )
->>>>>>> d66197bb226c76df947eb4a4f7c03eb581f59e79
+  );
 }
 
 export default App;
